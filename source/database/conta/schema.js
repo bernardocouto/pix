a24ab9ee3c276214a8fc94526710ab41f248cb22
Bernardo Couto <bernardocouto@icloud.com>
2020-08-14 19:11:43 -0300

    Adição da estrutura de relacionamento de titular pessoa física

--- source/database/conta/schema.js
+++ source/database/conta/schema.js
@@ -2,6 +2,11 @@ export default `
     type Conta {
         numero: String!
         agencia: Agencia @relation(name: "CONTA", direction: "IN")
-        pessoaFisica: [PessoaFisica] @relation(name: "PESSOA_FISICA", direction: "IN")
+        titularPessoaFisica: PessoaFisica @relation(name: "PESSOA_FISICA", direction: "IN")
+    }
+
+    type AddPessoaFisica {
+        from: PessoaFisica
+        to: Conta
     }
 `;
